f4357bf0e2f7c875cdb719afc697c50758ec62c8
Yogesh-160 <nityanandrekhajha@gmail.com>
2024-05-12 11:21:09 +0530

    server

--- server.js
+++ server.js
@@ -8,8 +8,17 @@ app.get('/',(req,res) => {
 })
 app.get('/home',function(req,res){
     res.send("Welcome to my hotel.... How can I help you?");
-    
-   
+})
+
+
+app.get('/idli', (req,res)=>{
+    var customized_idli = {
+        name: 'rava idli',
+        size: '10cm diameter',
+        is_sambhar : true,
+        is_coconut_chutney: false,
+    }
+    res.send(customized_idli);
 })
 
 app.listen(PORT, ()=>{
